3cce1cfaeff426791e1c18e74abc8bdee8c5d26f
0xhatsume <0xhatsume@gmail.com>
2024-04-06 18:44:09 +0800

    🔧 Update tailwind configs

---
+++ blobert-showdown/tailwind.config.js
@@ -0,0 +1,19 @@
+const flowbite = require("flowbite-react/tailwind");
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    // "./node_modules/flowbite/**/*.js",
+    // "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    //require('flowbite/plugin'),
+    flowbite.plugin(),
+  ],
+}
+
