6048aef6dac0729b4f16a97b98ea9a8d1a8b0531
0xhatsume <0xhatsume@gmail.com>
2024-04-06 18:45:37 +0800

    🌱 Update seed files and backup origins

--- blobert-showdown/src/App.tsx
+++ blobert-showdown/src/App.tsx
@@ -1,10 +1,12 @@
 import { useComponentValue } from "@dojoengine/react";
 import { Entity } from "@dojoengine/recs";
 import { useEffect, useState } from "react";
-import "./App.css";
 import { Direction } from "./utils";
 import { getEntityIdFromKeys } from "@dojoengine/utils";
 import { useDojo } from "./dojo/useDojo";
+
+import { BlobberSel } from "./components/index";
+import { Spinner } from "flowbite-react";
 
 function App() {
     const {
@@ -55,100 +57,49 @@ function App() {
     }, [clipboardStatus.message]);
 
     return (
-        <>
-            <button onClick={account?.create}>
-                {account?.isDeploying ? "deploying burner" : "create burner"}
-            </button>
-            {account && account?.list().length > 0 && (
-                <button onClick={async () => await account?.copyToClipboard()}>
-                    Save Burners to Clipboard
-                </button>
-            )}
-            <button onClick={handleRestoreBurners}>
-                Restore Burners from Clipboard
-            </button>
-            {clipboardStatus.message && (
-                <div className={clipboardStatus.isError ? "error" : "success"}>
-                    {clipboardStatus.message}
-                </div>
-            )}
+        <>  <div className="container mx-auto px-8 border border-red-500">
 
-            <div className="card">
-                <div>{`burners deployed: ${account.count}`}</div>
-                <div>
-                    select signer:{" "}
-                    <select
-                        value={account ? account.account.address : ""}
-                        onChange={(e) => account.select(e.target.value)}
-                    >
-                        {account?.list().map((account, index) => {
-                            return (
-                                <option value={account.address} key={index}>
-                                    {account.address}
-                                </option>
-                            );
-                        })}
-                    </select>
-                </div>
-                <div>
-                    <button onClick={() => account.clear()}>
-                        Clear burners
-                    </button>
-                    <p>
-                        You will need to Authorise the contracts before you can
-                        use a burner. See readme.
-                    </p>
-                </div>
-            </div>
+                {/* create burner/clear burner */}
+                <div className="flex py-2 text-orange-950">
 
-            <div className="card">
-                <button onClick={() => spawn(account.account)}>Spawn</button>
-                <div>
-                    Moves Left: {moves ? `${moves.remaining}` : "Need to Spawn"}
-                </div>
-                <div>
-                    Position:{" "}
-                    {position
-                        ? `${position.vec.x}, ${position.vec.y}`
-                        : "Need to Spawn"}
-                </div>
-            </div>
-
-            <div className="card">
-                <div>
-                    <button
-                        onClick={() =>
-                            position && position.vec.y > 0
-                                ? move(account.account, Direction.Up)
-                                : console.log("Reach the borders of the world.")
+                    <div className="">
+                        {account?.isDeploying ? 
+                        <div className="w-full flex items-center justify-center">
+                            <Spinner size="lg" 
+                            color="failure" aria-label="Summoning Blobber" />
+                        </div>
+                        :
+                        <button className={`
+                        ${account?.count >=4 ? 'bg-orange-900':'bg-orange-300'} 
+                        ${account?.count >=4 ? 'text-white':'text-orange-950'} 
+                        border-2 border-orange-950
+                        font-semibold
+                        px-2 py-2 rounded-lg w-full`}
+                        onClick={account?.create}
+                        disabled={account?.count >=4}
+                        > 
+                        {account?.count >=4 ? "U alredi hab so many Blobbers ser.": "Summon A Blobber"}</button>
                         }
+                    </div>
+
+                    <button className="bg-orange-300 
+                        border-2 border-orange-950
+                        font-semibold mx-2
+                        px-2 py-2 rounded-lg w-30"
+                    
+                    onClick={() => account.clear()}
                     >
-                        Move Up
-                    </button>
+                        
+                        Kick All Blobbers</button>
+                    
+                    <span className="flex items-center justify-end
+                    font-semibold ml-auto mr-2 px-2
+                    ">
+                        {`Summoned Blobbers: ${account.count}/4`}
+                    </span>
+
                 </div>
-                <div>
-                    <button
-                        onClick={() =>
-                            position && position.vec.x > 0
-                                ? move(account.account, Direction.Left)
-                                : console.log("Reach the borders of the world.")
-                        }
-                    >
-                        Move Left
-                    </button>
-                    <button
-                        onClick={() => move(account.account, Direction.Right)}
-                    >
-                        Move Right
-                    </button>
-                </div>
-                <div>
-                    <button
-                        onClick={() => move(account.account, Direction.Down)}
-                    >
-                        Move Down
-                    </button>
-                </div>
+                <BlobberSel/>
             </div>
         </>
     );
